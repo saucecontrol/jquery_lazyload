ff23606e3ea924eef2359bf23629863c86dacbd8
Clinton Ingram <clinton.ingram@outlook.com>
2014-06-23 16:22:49 -0700

    same grey pixel, compressed with pngout

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -31,7 +31,7 @@
             appear          : $.noop,
             load            : $.noop,
             error           : $.noop,
-            placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
+            placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAAAAAA6fptVAAAAFElEQVR4XgXAgQwAAACAsPy1LtYyAMUAxBPtybYAAAAASUVORK5CYII="
         };
 
         function update() {
